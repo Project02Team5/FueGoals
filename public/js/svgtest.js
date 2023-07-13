ef1206a99b8140d4626366c4f7f050dc96bea781
Andrew <ATHur1104@gmail.com>
2023-07-13 18:13:22 -0500

    adding notes to svgtest.js

--- public/js/svgtest.js
+++ public/js/svgtest.js
@@ -7,17 +7,20 @@ var rightEl = document.querySelector('.bodyViewR');
 var leftEl = document.querySelector('.bodyViewL');
 
 function bodyPartSelector(event) {
-  console.log(event.target.id);
-  leftEl.textContent = event.target.id;
-  if (event.target.id === 'head') {
-    rightEl.textContent = "Running";
-  } else if (event.target.id === 'upperBody') {
-    rightEl.textContent = "Bench Press";
-  } else if (event.target.id === 'legs') {
-    rightEl.textContent = "Squats";
-  } else if (event.target.id === 'core') {
-    rightEl.textContent = "Sit Ups";
-  }
+    console.log(event.target.id);
+    //   stats can be added here eventually
+    leftEl.textContent = event.target.id;
+
+    //   can use this to generate exercises on the right *modals for when you choose the exercise give it the correct params? maybe the params can be set by type as cardio usually is duration and other exercises are reps based.
+    if (event.target.id === 'head') {
+        rightEl.textContent = "Running";
+    } else if (event.target.id === 'upperBody') {
+        rightEl.textContent = "Bench Press";
+    } else if (event.target.id === 'legs') {
+        rightEl.textContent = "Squats";
+    } else if (event.target.id === 'core') {
+        rightEl.textContent = "Sit Ups";
+    }
 }
 
 headEl.addEventListener('click', bodyPartSelector);
