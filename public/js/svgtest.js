30add6eb3b4dfc996d98e3af35ef1e30d73082e3
Andrew <ATHur1104@gmail.com>
2023-07-13 16:30:58 -0500

    adding some logic into the svg diagram

--- public/js/svgtest.js
+++ public/js/svgtest.js
@@ -3,9 +3,21 @@ var headEl = document.querySelector('#head');
 var legsEl = document.querySelector('#legs');
 var upperBodyEl = document.querySelector('#upperBody');
 var coreEl = document.querySelector('#core');
+var rightEl = document.querySelector('.bodyViewR');
+var leftEl = document.querySelector('.bodyViewL');
 
 function bodyPartSelector(event) {
-    console.log(event.target.id);
+  console.log(event.target.id);
+  leftEl.textContent = event.target.id;
+  if (event.target.id === 'head') {
+    rightEl.textContent = "Running";
+  } else if (event.target.id === 'upperBody') {
+    rightEl.textContent = "Bench Press";
+  } else if (event.target.id === 'legs') {
+    rightEl.textContent = "Squats";
+  } else if (event.target.id === 'core') {
+    rightEl.textContent = "Sit Ups";
+  }
 }
 
 headEl.addEventListener('click', bodyPartSelector);
