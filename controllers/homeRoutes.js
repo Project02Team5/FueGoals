f02d1ef66c618410dca404d17198bb1925701c35
criatura435 <127697449+criatura435@users.noreply.github.com>
2023-07-15 12:00:40 -0500

    stats rendering based on login/out status

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,50 +1,125 @@
 const router = require("express").Router();
 const { User, Activity } = require("../models");
 const withAuth = require("../utils/auth");
-const { Op } = require("sequelize");
+const { Sequelize, Op } = require("sequelize");
 
 const intervalDate = (interval) => {
   var date = new Date();
-  var formatDate =  date.setDate(date.getDate() - interval);
+  var formatDate = date.setDate(date.getDate() - interval);
   return formatDate;
 };
 
 // Prevent non logged in users from viewing the homepage
-router.get("/", withAuth, async (req, res) => {
-
-  const interval = req.query.interval || 30
+router.get("/", async (req, res) => {
+  const interval = req.query.interval || 30;
   // http://localhost:3001/?interval=1
-
-  try {
-    const userData = await Activity.findAll({
-      include: {
-        model: User,
-        attributes: ["activity_interval"],
-      },
-      where: {
-        [Op.and]: [
-          { user_id: 4 },
-          {
-            createdAt: {
-              [Op.gt]: intervalDate(interval),
-            },
-          },
+  if (!req.session.logged_in) {
+    try {
+      const activityData = await Activity.findAll({
+        limit: 1,
+        attributes: [
+          [
+            Sequelize.literal('(SELECT COUNT(workout_completed) FROM activity WHERE workout_completed = true AND activity_type = "cardio")'),
+            "cardioCount",
+          ],
+          [
+            Sequelize.literal('(SELECT COUNT(workout_completed) FROM activity WHERE workout_completed = true AND activity_type = "strength")'),
+            "strengthCount",
+          ],
+          [
+            Sequelize.literal('(SELECT COUNT(workout_completed) FROM activity WHERE workout_completed = true AND activity_type = "flexibility")'),
+            "flexibilityCount",
+          ],
+          [
+            Sequelize.literal('(SELECT SUM(activity_duration) * SUM(activity_sets) FROM activity WHERE workout_completed = true AND activity_type = "cardio")'),
+            "cardioDuration",
+          ],
+          [
+            Sequelize.literal('(SELECT SUM(activity_duration) * SUM(activity_sets) FROM activity WHERE workout_completed = true AND activity_type = "strength")'),
+            "strengthDuration",
+          ],
+          [
+            Sequelize.literal('(SELECT SUM(activity_duration) * SUM(activity_sets) FROM activity WHERE workout_completed = true AND activity_type = "flexibility")'),
+            "flexibilityDuration",
+          ],
+          [
+            Sequelize.literal('(SELECT SUM(activity_sets) * SUM(strength_weight) FROM activity WHERE workout_completed = true AND activity_type = "strength")'),
+            "poundsLifted",
+          ],
         ],
-      },
-    });
+      });
+ 
+      const activities = activityData.map((activity) => activity.get({ plain: true }));
+      res.render("homepage", {
+        activities,
+        logged_in: req.session.logged_in,
+      });
+      // res.send(activities);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    try {
+      const activityData = await Activity.findAll({
+        limit: 1,
+        attributes: [
+          [
+            Sequelize.literal('(SELECT COUNT(workout_completed) FROM activity WHERE workout_completed = true AND activity_type = "cardio")'),
+            "cardioCount",
+          ],
+          [
+            Sequelize.literal('(SELECT COUNT(workout_completed) FROM activity WHERE workout_completed = true AND activity_type = "strength")'),
+            "strengthCount",
+          ],
+          [
+            Sequelize.literal('(SELECT COUNT(workout_completed) FROM activity WHERE workout_completed = true AND activity_type = "flexibility")'),
+            "flexibilityCount",
+          ],
+          [
+            Sequelize.literal('(SELECT SUM(activity_duration) * SUM(activity_sets) FROM activity WHERE workout_completed = true AND activity_type = "cardio")'),
+            "cardioDuration",
+          ],
+          [
+            Sequelize.literal('(SELECT SUM(activity_duration) * SUM(activity_sets) FROM activity WHERE workout_completed = true AND activity_type = "strength")'),
+            "strengthDuration",
+          ],
+          [
+            Sequelize.literal('(SELECT SUM(activity_duration) * SUM(activity_sets) FROM activity WHERE workout_completed = true AND activity_type = "flexibility")'),
+            "flexibilityDuration",
+          ],
+          [
+            Sequelize.literal('(SELECT SUM(activity_sets) * SUM(strength_weight) FROM activity WHERE workout_completed = true AND activity_type = "strength")'),
+            "poundsLifted",
+          ],
+        ],
+        include: {
+          model: User,
+          attributes: ["activity_interval", "name"],
+        },
+        where: {
+          [Op.and]: [
+            { user_id: 4 },
+            {
+              createdAt: {
+                [Op.gt]: intervalDate(interval),
+              },
+            },
+          ],
+        },
+      });
 
-    const users = userData.map((project) => project.get({ plain: true }));
+      const activities = activityData.map((activity) => activity.get({ plain: true }));
 
-    res.render('homepage', {
-      users,
-      // Pass the logged in flag to the template
-      logged_in: req.session.logged_in,
-      user_name: req.session.user_name,
-      user_id: req.session.user_id,
-    });
-    // res.send(users);
-  } catch (err) {
-    res.status(500).json(err);
+      res.render("homepage", {
+        activities,
+        logged_in: req.session.logged_in,
+        user_name: req.session.user_name,
+        user_id: req.session.user_id,
+      });
+      // res.send(activities);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
 });
 
@@ -58,7 +133,7 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.get('/dashboard', async (req, res) => {
+router.get("/dashboard", async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
       include: { model: Activity },
@@ -69,7 +144,7 @@ router.get('/dashboard', async (req, res) => {
       users = userData.get({ plain: true });
     }
 
-    res.render('dashboard', {
+    res.render("dashboard", {
       users,
       logged_in: req.session.logged_in,
     });
